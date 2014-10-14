962dbae20bf935e494a7915fa3952bc7a3859117
Tyrale <hello@tyrale.com>
2014-10-14 09:36:27 -0500

    Blog update at 2014-10-14 14:36:27

--- assets/js/must.js
+++ assets/js/must.js
@@ -1,40 +1,38 @@
-(function ($) {
-
-    $(document).ready(function(){
-
-    // ----------------------------------------------
-      
-    $( ".post-meta a:contains('yellow')" ).addClass('hidden');
-    $( ".post-meta a:contains('grey')" ).addClass('hidden');
-    $( ".post-meta a:contains('purple')" ).addClass('hidden');
-    $( ".post-meta a:contains('blue')" ).addClass('hidden');
-    $( ".post-meta a:contains('green')" ).addClass('hidden');
-    $( ".post-meta a:contains('red')" ).addClass('hidden');
-    $( ".post-meta a:contains('nowplaying')" ).addClass('hidden');
-    $( ".post-meta a:contains('black')" ).addClass('hidden');
-
-
-    $(".post")
-
-
-
-
-
+(function($) {
+    $(document).ready(function() {
+        // ----------------------------------------------
+        $(".post-meta a:contains('yellow')").addClass('hidden');
+        $(".post-meta a:contains('grey')").addClass('hidden');
+        $(".post-meta a:contains('purple')").addClass('hidden');
+        $(".post-meta a:contains('blue')").addClass('hidden');
+        $(".post-meta a:contains('green')").addClass('hidden');
+        $(".post-meta a:contains('red')").addClass('hidden');
+        $(".post-meta a:contains('nowplaying')").addClass('hidden');
+        $(".post-meta a:contains('black')").addClass('hidden');
+        $(".post")
     });
-
     // ----------------------------------------------
-
     $(".icon-rss").click(function(event) {
-    	/* Act on the event */
-    	mixpanel.track("RSS Subscriber");
+        /* Act on the event */
+        mixpanel.track("RSS Subscriber");
     });
     $(".tag-unlocks").click(function(event) {
-    	/* Act on the event */
-    	mixpanel.track("Unlocks");
+        /* Act on the event */
+        mixpanel.track("Unlocks");
     });
+    // ----------------------------------------------
+    var hasLooped = false;
 
-     // ----------------------------------------------
+    if(!hasLooped){
+        $( "div" ).each(function() {
+            $( ".bgImg" ).each(function(){
+                var $this = $(this);
+                $this.clone().prependTo($this.parent())
+            });
+            $(".bgImg").removeClass('bgImg');   
+        });
 
-
-
+        hasLooped = true;
+    }
+    
 })(jQuery);
