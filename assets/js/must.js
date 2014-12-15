75a219ecfb9511b4074b5aef00b12e06dd10e00f
Tyrale <hello@tyrale.com>
2014-12-15 15:53:58 -0500

    Blog update at 2014-12-15 20:53:58

--- assets/js/must.js
+++ assets/js/must.js
@@ -43,47 +43,25 @@
         hasLooped = true;
     }
 
-    var addCheck = false;
-    if(!addCheck){
-        $( ".post-header" ).each(function() {
-            $( ".add-check" ).each(function(){
-                var $this = $(this);
-                $this.prepend("<span class='playMarker'><a>✔︎</a></span>")
-            });
-            $(".add-check").removeClass('add-check');   
-        });
-        addCheck = true;
-    }
+    // var addCheck = false;
+    // if(!addCheck){
+    //     $( ".post-header" ).each(function() {
+    //         $( ".add-check" ).each(function(){
+    //             var $this = $(this);
+    //             $this.prepend("<span class='playMarker'><a>✔︎</a></span>")
+    //         });
+    //         $(".add-check").removeClass('add-check');   
+    //     });
+    //     addCheck = true;
+    // }
 
-    $('.post-header').click(function() {
-        $(this).parent().parent(".post").removeClass('played');
-    });
+    // $('.post-header').click(function() {
+    //     alert("post-header");
+    //     $(this).parent().parent().toggleClass('played');
+    // });
     $('.playMarker').click(function() {
-        $(this).parent().parent().parent(".post").addClass('played');
-    });
-    
-
-
-    var menu = $('#navigation-menu');
-    var menuToggle = $('#js-mobile-menu');
-    var signUp = $('.sign-up');
-
-    $(menuToggle).on('click', function(e) {
-    e.preventDefault();
-    menu.slideToggle(function(){
-      if(menu.is(':hidden')) {
-        menu.removeAttr('style');
-      }
-    });
-    });
-
-    // underline under the active nav item
-    $(".nav .nav-link").click(function() {
-    $(".nav .nav-link").each(function() {
-      $(this).removeClass("active-nav-item");
-    });
-    $(this).addClass("active-nav-item");
-    $(".nav .more").removeClass("active-nav-item");
+        alert("playmaker");
+        $(this).parent().parent().parent().toggleClass('played');
     });
 
     
